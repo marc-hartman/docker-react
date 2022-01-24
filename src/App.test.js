c1992c37a5c2cbc9d2f4bd7a6455d02c7cf1b708
marc <marc.hartman@kinandcarta.com>
2022-01-24 15:38:08 -0500

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,18 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Bye there/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/just changed/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('render', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/just changed/i);
   expect(linkElement).toBeInTheDocument();
 });
